6a17d81c5822e2499bbccb390922ec329b5f15bb
MaximusVid <davidpermana47@gmail.com>
2022-11-22 20:46:37 +0700

    fix main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'JemputAh App',
-      home: const AddressDetailPage(),
+      home: const SplashScreen(),
       theme: ThemeData(
         primarySwatch: Colors.blue,
         fontFamily: 'Poppins',
