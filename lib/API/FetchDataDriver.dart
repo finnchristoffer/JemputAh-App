08027286f116ffddd9d4248d14689cce57b6b593
ThaddeusEvan <98073989+ThaddeusEvan@users.noreply.github.com>
2022-12-21 21:47:03 +0700

    Penjemputan Screen DB Integration

---
+++ lib/API/FetchDataDriver.dart
@@ -0,0 +1,20 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:jemputah_app/models/driver.dart';
+import 'package:jemputah_app/models/jemput.dart';
+
+class FetchDataDriver {
+  Future<List<Map<String, dynamic>>> fetchListDriver(int? timecode) async {
+    List<Map<String, dynamic>> dataList = [];
+    var query = await FirebaseFirestore.instance
+        .collection("driver")
+        .where("slot_$timecode", isEqualTo: "")
+        .get();
+    List<DocumentSnapshot> documents = query.docs;
+    for (DocumentSnapshot snapshot in documents) {
+      Map<String, dynamic> data = {};
+      data = Driver().snap(snapshot);
+      dataList.add(data);
+    }
+    return dataList;
+  }
+}
